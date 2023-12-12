34612ea68a831a43c3991058c7ff4d566f30b59f
Your Name <you@example.com>
2023-12-12 03:17:35 -0500

    first commit

---
+++ src/main/java/com/lizana/clienteproducto/ClienteProductoApplication.java
@@ -0,0 +1,13 @@
+package com.lizana.clienteproducto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteProductoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lizana/clienteproducto/model/PerfilUser.java
@@ -0,0 +1,14 @@
+package com.lizana.clienteproducto.model;
+
+
+public class PerfilUser {
+
+    private String tipoDeDocumento;
+    private String numeroDeDocumento;
+    private String tipodeproducto;
+    private String Subclase;
+    private Number titular;
+    private Number firmante;
+
+
+}
